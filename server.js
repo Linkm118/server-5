313aa79cba55930cbd6b7025e9150b6f5f35bb1c
Linkm118 <206734997+Linkm118@users.noreply.github.com>
2025-10-31 12:15:14 +0900

    postとputの修正

--- server.js
+++ server.js
@@ -5,8 +5,30 @@ const app = new Hono();
 app.use('/*', serveStatic({ root: './public' }));
 
 // GETリクエストに対する処理
-app.get('/api', async (c) => {
-  return c.json({ message: 'GET' });
+app.get('/api/:name/:rank', async (c) => {
+  const name = c.req.param('name');
+  const rank = c.req.param('rank');
+  return c.json({ message: 'GET', name, rank });
+});
+
+// POSTリクエストに対する処理
+app.post('/api', async (c) => {
+  // メッセージボディの受け取り
+  const body = await c.req.parseBody();
+  const name = body['name'];
+  const rank = body['rank'];
+
+  return c.json({ message: 'POST', form: { name, rank } });
+});
+
+// PUTリクエストに対する処理
+app.put('/api', async (c) => {
+  // メッセージボディの受け取り
+  const body = await c.req.json();
+  const name = body['name'];
+  const rank = body['rank'];
+
+  return c.json({ message: 'PUT', json: { name, rank } });
 });
 
 Deno.serve(app.fetch);
